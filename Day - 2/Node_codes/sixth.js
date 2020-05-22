ca2c1a36555e160c902fe5ccac5daaffd57e8f56
SakshamB28 <56152493+SakshamB28@users.noreply.github.com>
2020-05-22 23:22:32 +0530

    Delete sixth.js

--- Day - 2/Node_codes/sixth.js
+++
@@ -1,7 +0,0 @@
-//server example
-
-var http = require('http');
-http.createServer(function(req,res){
-    res.write('Hello, Mr. Saksham Bisht');
-    res.end();  // end the response process
-}).listen(8080);
